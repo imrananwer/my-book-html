5e47411b372db0b0bbd3a1571224692c62440391
imrananwer <emraanapple10@gmail.com>
2025-12-10 23:12:04 +0500

    Fix sidebar links to static/book

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   tutorialSidebar: [
-    { type: 'link', label: 'A Home', href: '/book/index.html' },
-    { type: 'link', label: 'Chapter 1 — Introduction', href: '/book/chapter1.html' },
-    { type: 'link', label: 'Chapter 2 — Human-Robot Interaction', href: '/book/chapter2.html' },
+    { type: 'link', label: 'Home', href: '/static/book/index.html' },
+    { type: 'link', label: 'Chapter 1 — Introduction', href: '/static/book/chapter1.html' },
+    { type: 'link', label: 'Chapter 2 — Human-Robot Interaction', href: '/static/book/chapter2.html' },
   ],
 };
